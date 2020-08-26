e4dbf390c4321aecd37a5c5454ad4fef73234cfb
luigiRP <luigirosales84@gmail.com>
2020-08-26 14:12:50 +0000

    Added Twitter component and styles

--- src/js/component/twitter.js
+++ src/js/component/twitter.js
@@ -1,37 +1,34 @@
 import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import "../../styles/twitter.scss";
 
-export default function Twitter() {
+export default function Twitter(props) {
 	return (
 		<Fragment>
-			<div className="container border preview">
+			<div className="container border preview-twitter">
 				<div className="row py-3 px-3 d-flex justify-content-between align-items-center">
 					<div className="col d-flex align-items-center">
 						<img
-							src="https://images.pexels.com/photos/3028448/pexels-photo-3028448.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+							src={props.profile}
 							alt="profile"
-							className="rounded-circle profile"
+							className="rounded-circle profile-twitter"
 							width="60"
 							height="60"
 						/>
-						<div className="mx-3 d-flex align-items-baseline mt-3">
-							<h4 className="mb-0 mr-3 username">
-								<strong>Luigi</strong>
+						<div className="mx-3 d-flex align-items-baseline mt-3 container-twitter">
+							<h4 className="mb-0 mr-3 username-twitter">
+								<strong>{props.name}</strong>
 							</h4>
-							<p className="text-secondary details">@luigirp - 0 min</p>
+							<p className="text-secondary details-twitter">@{props.username} - 0 min</p>
 						</div>
 					</div>
-					<div className="arrow">
+					<div className="arrow-twitter">
 						<i className="fas fa-chevron-down text-secondary mr-5" />
 					</div>
 					<div className="row py-3 px-3">
-						<h4 className="tweet">
-							No me puedo creer que el 2-8 vaya a ser su último partido y que todo termine con una disputa
-							entre abogados. Es que es demencial. El mejor jugador de la historia del club saliendo del
-							club de esta manera. Ni en la peor de nuestras pesadillas. Costará asimilarlo.
-						</h4>
+						<h4 className="tweet">{props.tweet}</h4>
 					</div>
-					<div className="row d-flex justify-content-between w-100 text-secondary icons">
+					<div className="row d-flex justify-content-between w-100 text-secondary icons-twitter">
 						<h4>
 							<i className="col far fa-comment" />
 						</h4>
@@ -50,3 +47,10 @@ export default function Twitter() {
 		</Fragment>
 	);
 }
+
+Twitter.propTypes = {
+	name: PropTypes.string,
+	username: PropTypes.string,
+	tweet: PropTypes.string,
+	profile: PropTypes.object
+};
