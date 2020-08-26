c2134e3988153fdf0adc0176e22497ad0942109a
luigiRP <luigirosales84@gmail.com>
2020-08-26 12:01:10 +0000

    Added Instagram Component@

--- src/js/component/instagram.js
+++ src/js/component/instagram.js
@@ -5,27 +5,27 @@ import "../../styles/instagram.scss";
 export default function Instagram(props) {
 	return (
 		<Fragment>
-			<div className="container border calendar">
-				<div className="row d-flex align-items-center justify-content-between px-3 top">
-					<div className="col-3 d-flex justify-content-around py-3 align-items-center ">
+			<div className="container border preview-instagram">
+				<div className="row d-flex align-items-center justify-content-between top">
+					<div className="col d-flex justify-content-start py-3 align-items-center ">
 						<img
 							src={props.imgProfile}
 							alt="profile-pic"
-							className="rounded-circle mr-4 profile"
+							className="rounded-circle mr-4 profile-instagram"
 							width="40"
 							height="40"
 						/>
-						<h5 className="username">{props.name}</h5>
+						<h5 className="username-instagram">{props.name}</h5>
 					</div>
-					<div className="d-flex justify-content-end">
-						<i className="fas fa-ellipsis-h " />
+					<div className="col d-flex justify-content-end">
+						<i className="fas fa-ellipsis-h options-instagram" />
 					</div>
 				</div>
 				<div className="row">
 					<img src={props.imgPost} alt="post-pic" className="w-100 h-50" />
 				</div>
-				<div className="row d-flex justify-content-between px-3">
-					<div className="d-flex align-items-center col-5 justify-content-between py-2 left-bottom">
+				<div className="row d-flex justify-content-between icons-instagram">
+					<div className="d-flex align-items-center col justify-content-around py-2">
 						<h3>
 							<i className="fas fa-heart" />
 						</h3>
@@ -36,7 +36,7 @@ export default function Instagram(props) {
 							<i className="far fa-paper-plane" />
 						</h3>
 					</div>
-					<div className=" d-flex justify-content-end align-items-center">
+					<div className=" d-flex col justify-content-end align-items-center">
 						<h3>
 							<i className="far fa-bookmark" />
 						</h3>
@@ -44,7 +44,7 @@ export default function Instagram(props) {
 				</div>
 				<div className="bottom">
 					<div className="row">
-						<div className="col-6">
+						<div className="col">
 							<p>1937 likes</p>
 						</div>
 					</div>
