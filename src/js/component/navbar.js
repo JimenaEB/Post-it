545f1b11ba537b82c39c9330ca6d94a839e000ce
avalentinagd <avalentinagd@gmail.com>
2020-08-27 10:08:18 +0000

    Added logo in navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Logo } from "../component/logo";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			<Link to="/logo">
+				<span className="navbar-brand mb-0 h1">
+					<Logo />
+				</span>
 			</Link>
-			<div className="ml-auto">
+
+			{/* <div className="ml-auto">
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
-			</div>
+			</div> */}
 		</nav>
 	);
 };
