ef5e8c05a8435d901ff10354c2a466fa7a1ba223
avalentinagd <avalentinagd@gmail.com>
2020-08-27 10:11:39 +0000

    Login view design

---
+++ src/js/views/login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../../styles/login.scss";
+import { InputIconBox } from "../component/inputiconbox";
+
+export const Login = props => (
+	<div className="text-center mt-5">
+		<h2 className="LoginTitle">Log-in</h2>
+		<br />
+		<form>
+			<div className="EmailInput flex-container">
+				<span>
+					<InputIconBox to="/inputiconbox" iconBox="fas fa-unlock" />
+				</span>
+				<input className="InputE ml-1" type="text" id="Email" name="Email" placeholder="Email" />
+			</div>
+			<br />
+			<div className="PasswordInput flex-container">
+				<span>
+					<InputIconBox to="/inputiconbox" iconBox="far fa-envelope" />
+				</span>
+				<input className="InputP ml-1" type="text" id="Password" name="Password" placeholder="Password" />
+			</div>
+		</form>
+		<br />
+
+		<button className="LoginButton">Sign in!</button>
+		<br />
+		<br />
+		<a href="#" className="linkRegister">
+			If you do not have an account, please register here
+		</a>
+		<br />
+		<br />
+		<br />
+		<br />
+		<div>
+			<span className="font-weight-bold">Sign in with:</span>
+			<br />
+			<div>
+				<i className="InstagramLogo fab fa-instagram" />
+				<i className="TwitterLogo fab fa-twitter" />
+				<i className="LinkedinLogo fab fa-linkedin-in" />
+				<i className="FacebookLogo fab fa-facebook-f" />
+				<br />
+				<br />
+			</div>
+		</div>
+	</div>
+);
