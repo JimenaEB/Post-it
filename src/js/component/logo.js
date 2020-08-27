6e7d4205aa00d6e0733566dbbda589a69a4b47f4
avalentinagd <avalentinagd@gmail.com>
2020-08-27 10:06:37 +0000

    Logo creation

---
+++ src/js/component/logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/logo.scss";
+
+export const Logo = () => {
+	return (
+		<div>
+			<img
+				className="logo"
+				src="https://i.pinimg.com/originals/72/39/a9/7239a93dbb7a4fd788e58e8b4ad0cc53.jpg"
+				alt="Logo Post It"
+			/>
+		</div>
+	);
+};
