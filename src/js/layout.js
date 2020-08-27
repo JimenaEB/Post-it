a728f757e4c249aa121bf9466f6d06f43896c19e
avalentinagd <avalentinagd@gmail.com>
2020-08-27 10:16:43 +0000

    Add login in layout

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
+import { Login } from "./views/login";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+//import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -23,7 +23,7 @@ const Layout = () => {
 					<Navbar />
 					<Switch>
 						<Route exact path="/">
-							<Home />
+							<Login />
 						</Route>
 						<Route exact path="/demo">
 							<Demo />
@@ -35,7 +35,7 @@ const Layout = () => {
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
-					<Footer />
+					{/* <Footer /> */}
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
