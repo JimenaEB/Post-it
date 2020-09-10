9b2d1e3ddc6e3b8d0f0c3e0a586ab8ac8f5823ff
luigiRP <luigirosales84@gmail.com>
2020-09-10 10:22:12 +0000

    Added Lables and fixed layout

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -22,33 +22,38 @@ export const Register = () => {
 		<div className="text-center mt-5">
 			<h2 className="NewUserTitle">New User</h2>
 			<br />
-			<form>
-				<div className="PasswordInput flex-container">
-					<span>
+			<form className="container-form container">
+				<div className="PasswordInput flex-container  ">
+					<span className="">
 						<InputIconBox to="/inputiconbox" iconBox="fas fa-comment" />
 					</span>
+					<label className="mx-3 col-1 mr-5">Name:</label>
 					<input className="InputP ml-1" type="text" id="Name" name="name" placeholder="Name" />
 				</div>
+
 				<br />
 				<div className="UsernameInput flex-container">
 					<span>
 						<InputIconBox to="/inputiconbox" iconBox="fas fa-user-alt" />
 					</span>
-					<input className="InputU ml-1" type="text" id="Username" name="Username" placeholder="Username" />
+					<label className="mx-3 col-1 mr-5">Username:</label>
+					<input className="InputU ml-1 " type="text" id="Username" name="Username" placeholder="Username" />
 				</div>
 				<br />
-				<div className="EmailInput flex-container">
+				<div className="EmailInput flex-container ">
 					<span>
-						<InputIconBox to="/inputiconbox" iconBox="fas fa-unlock" />
+						<InputIconBox to="/inputiconbox" iconBox="far fa-envelope " />
 					</span>
+					<label className="mx-3 col-1 mr-5">Email:</label>
 					<input className="InputE ml-1" type="text" id="Email" name="Email" placeholder="Email" />
 				</div>
 				<br />
-				<div className="PasswordInput flex-container">
+				<div className="PasswordInput flex-container ">
 					<span>
-						<InputIconBox to="/inputiconbox" iconBox="far fa-envelope" />
+						<InputIconBox to="/inputiconbox" iconBox="fas fa-unlock" />
 					</span>
-					<input className="InputP ml-1" type="text" id="Password" name="Password" placeholder="Password" />
+					<label className="mx-3 col-1 mr-5">Password:</label>
+					<input className="InputP ml-1 " type="text" id="Password" name="Password" placeholder="Password" />
 				</div>
 				<br />
 
